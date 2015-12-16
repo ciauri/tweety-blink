70698041de6aef6e11bc2d8ba560c5ead3a6f59a
Stephen Ciauri <stephen.ciauri@gmail.com>
2015-12-15 20:58:12 -0800

    RGB LED

--- app/listener/listener.py
+++ app/listener/listener.py
@@ -6,7 +6,9 @@ import time
 # Configure the Pi to use the BCM (Broadcom) pin names, rather than the pin positions
 GPIO.setmode(GPIO.BCM)
 
-red_pin = 18
+red_pin = 27
+blue_pin = 18
+green_pin = 17
 
 GPIO.setup(red_pin, GPIO.OUT)
 
@@ -15,14 +17,21 @@ class TweetListener(StreamListener):
     def on_status(self, status):
         print("Tweet: {}".format(status.text))
         GPIO.setmode(GPIO.BCM)
-        red_pin = 18
         GPIO.setup(red_pin, GPIO.OUT)
+        GPIO.setup(green_pin, GPIO.OUT)
+        GPIO.setup(blue_pin, GPIO.OUT)
         try:
-            GPIO.output(red_pin, True)  # LED on
-            time.sleep(0.5)             # delay 0.5 seconds
-            GPIO.output(red_pin, False) # LED off
-            time.sleep(0.5)             # delay 0.5 seconds
+                if "#yolo" in status.text:
+                    pin = red_pin
+                elif "#blessed" in status.text:
+                    pin = blue_pin
+                else:
+                    pin = green_pin
+
+                GPIO.output(pin, True)  # LED on
+                time.sleep(0.5)             # delay 0.5 seconds
+                GPIO.output(pin, False) # LED off
         finally:
-            print("Cleaning up")
-            GPIO.cleanup()
-            # And anything else we want to do AKA make light blink or whatever
+                print("Cleaning up")
+                GPIO.cleanup()
+                # And anything else we want to do AKA make light blink or whatever
